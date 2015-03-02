fc459f80191f35980e03c4595078fd64ab396317
Nesim <nesim.avdullahu@gmail.com>
2015-03-02 08:47:49 +0100

    Create Stop_Prod_VM.ps1

---
+++ Stop_Prod_VM.ps1
@@ -0,0 +1,41 @@
+workflow Stop_Prod_VM
+{
+    Param
+    (   
+        [parameter(Mandatory=$true)] 
+        [String] 
+        $VMName,   
+        
+        [parameter(Mandatory=$true)] 
+        [String] 
+        $ServiceName      
+    )
+    
+    $day = (Get-Date).DayOfWeek
+    if ($day -eq 'Saturday' -or $day -eq 'Sunday'){
+        exit
+    }
+    
+    $AzureConnectionName = Get-AutomationVariable -Name "AzureConnectionName"
+    $subscriptionName = Get-AutomationVariable -Name "SubscriptionName" 
+
+    
+    # Get the Azure connection asset that is stored in the Auotmation service based on the name that was passed into the runbook 
+    $AzureConn = Get-AutomationConnection -Name $AzureConnectionName
+    if ($AzureConn -eq $null)
+    {
+        throw "Could not retrieve '$AzureConnectionName' connection asset. Check that you created this first in the Automation service."
+    }
+
+    # Get the Azure management certificate that is used to connect to this subscription
+    $Certificate = Get-AutomationCertificate -Name $AzureConn.AutomationCertificateName
+    if ($Certificate -eq $null)
+    {
+        throw "Could not retrieve '$AzureConn.AutomationCertificateName' certificate asset. Check that you created this first in the Automation service."
+    }
+
+    # Set the Azure subscription configuration
+    Set-AzureSubscription -SubscriptionName $AzureConnectionName -SubscriptionId $AzureConn.SubscriptionID -Certificate $Certificate
+    Select-AzureSubscription $subscriptionName
+    Stop-AzureVM -ServiceName $ServiceName -Name $VMName -Force
+}
